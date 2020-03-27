5f1e070a97743844bb7c75e26e985203f3214630
LindsayMF <meneslindsay@hotmail.com>
2020-03-26 20:46:07 -0600

    Added the file for a search bar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,17 @@
 import React from 'react';
 //import css files here
 
-function SearchBar({ handleSearchChange }) {
+function SearchBar({ handleSearchChange, handleFormSubmit }) {
     return(
         <div className="searchbar">
             <form className="form-inline">
                 <input 
                 className="form-control"
                 type="search"
-                placeholder="Enter Name"
+                placeholder="Enter Employee Name"
                 onChange={
                     e => handleSearchChange(e)
                 }>
-                
                 </input>
             </form>
         </div>
